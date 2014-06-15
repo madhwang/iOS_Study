505190d5f932d7c4c29c73fe07804565c83086f5
madhwang <madhwang@gmail.com>
2014-06-15 19:36:10 +0900

    두번째 페이지2

--- Timecard/Timecard/TimeCardViewController.h
+++ Timecard/Timecard/TimeCardViewController.h
@@ -11,11 +11,26 @@
 @interface TimeCardViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UITextField *memberNo;
-
 @property (weak, nonatomic) IBOutlet UITextField *password;
 
+
+@property (retain, nonatomic) NSURLConnection *connection;
+@property (retain, nonatomic) NSMutableData *receivedData;
+
+
+/* 엔터 클릭시 키보드 숨김 */
 - (IBAction) textFieldReturn:(id)sender;
 
+/* 백그라운드 터치시 키보드 숨김 */
 - (IBAction) backgroundTouched:(id)sender;
+
+/* 인증하기 버튼 터치시 */
+- (IBAction) authConfirmBtnTouched:(id)sender;
+
+
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;
+
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection;
 
 @end
--- Timecard/Timecard/TimeCardViewController.m
+++ Timecard/Timecard/TimeCardViewController.m
@@ -14,16 +14,63 @@
 
 @implementation TimeCardViewController
 
-/* 엔터키 클릭시 키보드 숨김 */
 -(IBAction) textFieldReturn:(id)sender
 {
     [sender resignFirstResponder];
 }
 
-/* 백그라운드 클릭시 키보드 숨김 */
 - (IBAction) backgroundTouched:(id)sender
 {
     [_memberNo resignFirstResponder];
     [_password resignFirstResponder];
+}
+
+
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    [_receivedData appendData:data];
+}
+
+/*
+ if there is an error occured, this method will be called by connection
+ */
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    
+    NSLog(@"%@" , error);
+}
+
+/*
+ if data is successfully received, this method will be called by connection
+ */
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    
+    //initialize convert the received data to string with UTF8 encoding
+    NSString *htmlSTR = [[NSString alloc] initWithData:self.receivedData
+                                              encoding:NSUTF8StringEncoding];
+
+    if([htmlSTR isEqualToString:@"OK"] == true)
+    {
+        // 화면 이동
+        //멤버 번호를 가지고 이동
+    }
+    
+   
+}
+
+
+-(IBAction) authConfirmBtnTouched:(id)sender
+{
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://localhost:8080/authConfirm?memberNo=%@&passwd=%@",_memberNo,_password]];
+    
+    
+    //initialize a request from url
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+    //initialize a connection from request
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    self.connection = connection;
+
+    
+    //start the connection
+    [connection start];
 }
 @end
